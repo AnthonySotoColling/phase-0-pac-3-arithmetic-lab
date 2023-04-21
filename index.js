1d9a80cd538f1a9bd5346a4fe50ca3328b77fe32
anthonyLuisSotoColling <govips@gmail.com>
2023-04-20 22:30:26 -0700

    lab done

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+    return a + b
+}
+add(1500, 5)
+function subtract(a, b) {
+    return a - b
+}
+subtract(0, -130)
+function multiply(a, b) {
+    return a * b
+}
+multiply(7701, 10)
+function divide(a, b) {
+    return a / b
+}
+divide(5.401234567901234, 10)
+function increment(n) {
+    return n + 1
+}
+increment(538)
+function decrement(n) {
+    return n - 1
+}
+decrement(555)
+function makeInt(n) {
+    return parseInt(n, 10)
+}
+makeInt("20")
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
+preserveDecimal("2.2222")
